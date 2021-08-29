91ad3251d5b957aed1dc3cd9b06c2eed282f511f
wilfredo <wilfredofrias146@gmail.com>
2021-08-29 17:12:32 -0300

    massiel

---
+++ .next/server/pages/index.js
@@ -0,0 +1,355 @@
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 529:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ pages)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(282);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(297);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(675);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(664);
+;// CONCATENATED MODULE: ./public/desktop/logo.svg
+/* harmony default export */ const logo = ({"src":"/_next/static/image/public/desktop/logo.5b4e652aff88b09ef8f1d465f3db445b.svg","height":32,"width":192});
+;// CONCATENATED MODULE: ./public/desktop/image-interactive.jpg
+/* harmony default export */ const image_interactive = ({"src":"/_next/static/image/public/desktop/image-interactive.1d330522c9ca6cf07a9ca77bafbbbd6b.jpg","height":500,"width":731,"blurDataURL":"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/sBCgoKCgoKCwwMCw8QDhAPFhQTExQWIhgaGBoYIjMgJSAgJSAzLTcsKSw3LVFAODhAUV5PSk9ecWVlcY+Ij7u7+//CABEIAAUACAMBIgACEQEDEQH/xAAUAAEAAAAAAAAAAAAAAAAAAAAB/9oACAEBAAAAAD//xAAUAQEAAAAAAAAAAAAAAAAAAAAF/9oACAECEAAAAH//xAAUAQEAAAAAAAAAAAAAAAAAAAAF/9oACAEDEAAAAGf/xAAbEAACAwADAAAAAAAAAAAAAAABBAIDEQAxMv/aAAgBAQABPwBlti16yWwAisJedPeEbz//xAAaEQEAAQUAAAAAAAAAAAAAAAABAAIDEzGx/9oACAECAQE/ALFI5BNJwn//xAAXEQADAQAAAAAAAAAAAAAAAAAAA0Jy/9oACAEDAQE/AFXo/9k="});
+;// CONCATENATED MODULE: ./public/icon-facebook.svg
+/* harmony default export */ const icon_facebook = ({"src":"/_next/static/image/public/icon-facebook.e9773e52bc64ea377cb95af7ccb6877e.svg","height":24,"width":24});
+;// CONCATENATED MODULE: ./public/icon-twitter.svg
+/* harmony default export */ const icon_twitter = ({"src":"/_next/static/image/public/icon-twitter.5dc7711ff2433b3b89ee7742a407e3bd.svg","height":20,"width":24});
+;// CONCATENATED MODULE: ./public/icon-pinterest.svg
+/* harmony default export */ const icon_pinterest = ({"src":"/_next/static/image/public/icon-pinterest.61a3584463b61d0f0d6bc14bc5d1e40a.svg","height":24,"width":24});
+;// CONCATENATED MODULE: ./public/icon-instagram.svg
+/* harmony default export */ const icon_instagram = ({"src":"/_next/static/image/public/icon-instagram.8d0df55595e16bac2d486efccdc6b4b3.svg","height":24,"width":24});
+;// CONCATENATED MODULE: ./pages/index.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Inicio = () => {
+  return /*#__PURE__*/(0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+    children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("header", {
+      className: "hero",
+      children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "contenedor",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "logo",
+          children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+            src: logo,
+            className: "logo"
+          })
+        }), /*#__PURE__*/jsx_runtime_.jsx("nav", {
+          className: "menu",
+          children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("ul", {
+            children: [/*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+              href: "",
+              children: "About"
+            }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+              href: "",
+              children: "Services"
+            }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+              href: "",
+              children: "Projects"
+            }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+              href: "",
+              children: "Contact"
+            })]
+          })
+        })]
+      }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+        className: "header-contenido",
+        children: /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "contenedor2",
+          children: /*#__PURE__*/jsx_runtime_.jsx("h1", {
+            className: "immersive",
+            children: "IMMERSIVE EXPERIENCES THAT DELIVER"
+          })
+        })
+      })]
+    }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+      className: "contenedor",
+      children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("main", {
+        className: "section",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "fotoSection",
+          children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+            src: image_interactive,
+            className: "logo"
+          })
+        }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: "textoGafa",
+          children: [/*#__PURE__*/jsx_runtime_.jsx("h1", {
+            children: "THE LEADER IN INTERACTIVE VR"
+          }), /*#__PURE__*/jsx_runtime_.jsx("p", {
+            className: "",
+            children: "Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand."
+          })]
+        })]
+      })
+    }), /*#__PURE__*/jsx_runtime_.jsx("h1", {
+      className: "creations",
+      children: "OUR CREATIONS"
+    }), /*#__PURE__*/jsx_runtime_.jsx("article", {
+      className: "Galerias",
+      children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "contenedor",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto1"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto2"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto3"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto4"
+        })]
+      })
+    }), /*#__PURE__*/jsx_runtime_.jsx("article", {
+      className: "article2",
+      children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "contenedor",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto5"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto6"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto7"
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "foto8"
+        })]
+      })
+    }), /*#__PURE__*/jsx_runtime_.jsx("footer", {
+      className: "footerclases",
+      children: /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+        className: "classFooter",
+        children: [/*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "logofooter",
+          children: /*#__PURE__*/jsx_runtime_.jsx(next_image.default, {
+            src: logo,
+            className: "logo"
+          })
+        }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("ul", {
+          children: [/*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "",
+            children: "About"
+          }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "",
+            children: "Services"
+          }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "",
+            children: "Projects"
+          }), /*#__PURE__*/jsx_runtime_.jsx(next_link.default, {
+            href: "",
+            children: "Contact"
+          })]
+        }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("div", {
+          className: "LogoSociales",
+          children: [/*#__PURE__*/jsx_runtime_.jsx("a", {
+            href: "!#",
+            children: /*#__PURE__*/jsx_runtime_.jsx("img", {
+              src: icon_facebook,
+              alt: ""
+            })
+          }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+            href: "!#",
+            children: /*#__PURE__*/jsx_runtime_.jsx("img", {
+              src: icon_twitter,
+              alt: ""
+            })
+          }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+            href: "!#",
+            children: /*#__PURE__*/jsx_runtime_.jsx("img", {
+              src: icon_pinterest,
+              alt: ""
+            })
+          }), /*#__PURE__*/jsx_runtime_.jsx("a", {
+            href: "!#",
+            children: /*#__PURE__*/jsx_runtime_.jsx("img", {
+              src: icon_instagram,
+              alt: ""
+            })
+          })]
+        }), /*#__PURE__*/jsx_runtime_.jsx("div", {
+          className: "subtitulo",
+          children: /*#__PURE__*/jsx_runtime_.jsx("p", {
+            children: "@Loopstudios. All rights reserved"
+          })
+        })]
+      })
+    })]
+  });
+};
+
+/* harmony default export */ const pages = (Inicio);
+
+/***/ }),
+
+/***/ 325:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 822:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/image-config.js");
+
+/***/ }),
+
+/***/ 695:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head.js");
+
+/***/ }),
+
+/***/ 378:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 162:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 773:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 248:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 372:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 665:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 747:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 333:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 456:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 556:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/to-base-64.js");
+
+/***/ }),
+
+/***/ 620:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 297:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 282:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 431:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [61], () => (__webpack_exec__(529)));
+module.exports = __webpack_exports__;
+
+})();
